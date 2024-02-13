3984e95d813ea1f65aa28c5676928f2f82eca2df
WealthyMole101 <WealthyMole101@users.noreply.github.com>
2024-02-13 19:49:16 +0000

    deploy: 637c45b9ac5856e8a7028b98d4e6d2dd51baf284

---
+++ search/all_2.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['get_5fcoordinates_0',['get_coordinates',['../classcircle.html#a5dbc4e0f96bf7ab9564b1c37b8718fdc',1,'circle::get_coordinates()'],['../classgraphic__primitive.html#aa6bece6061f07730c044d16ff11802b7',1,'graphic_primitive::get_coordinates()'],['../classrectangle.html#af65255dd80e09c9bccd57ae0163d1691',1,'rectangle::get_coordinates()'],['../classsquare.html#ac2638f7131e9b0a409c7629f2f7a7d32',1,'square::get_coordinates()'],['../classtriangle.html#a195580f17351c0b8e0ada7c4eea2f00b',1,'triangle::get_coordinates()']]],
+  ['graphic_5fprimitive_1',['graphic_primitive',['../classgraphic__primitive.html',1,'graphic_primitive'],['../classgraphic__primitive.html#a2a3db778d9aeb68bcbf52a4e1b99bb0a',1,'graphic_primitive::graphic_primitive()']]],
+  ['graphic_5fprimitive_2eh_2',['graphic_primitive.h',['../graphic__primitive_8h.html',1,'']]],
+  ['graphic_5fview_3',['graphic_view',['../classgraphic__view.html',1,'']]],
+  ['graphic_5fview_2eh_4',['graphic_view.h',['../graphic__view_8h.html',1,'']]],
+  ['graphics_5feditor_5',['graphics_editor',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ search/functions_4.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['set_5fbackground_5fcolor_0',['set_background_color',['../classcircle.html#ab741f430afbd31e31f6256d41092d1ef',1,'circle::set_background_color()'],['../classgraphic__primitive.html#a2e9e6b7986f906141ac6cf345a8b1a6e',1,'graphic_primitive::set_background_color()'],['../classrectangle.html#aab5c2c90afc7171367193d34eaa7abeb',1,'rectangle::set_background_color()'],['../classsquare.html#a3b9780f74b7d92900cf53a0aeb7f3733',1,'square::set_background_color()'],['../classtriangle.html#ab85611a0b68dce87b398f2ff446c4b70',1,'triangle::set_background_color()']]],
+  ['set_5fcoordinates_1',['set_coordinates',['../classcircle.html#a90d0d439c662203756aa3dd7fc573c7b',1,'circle::set_coordinates()'],['../classgraphic__primitive.html#aabb6fc7877b181cbe53163c3424d21c9',1,'graphic_primitive::set_coordinates()'],['../classrectangle.html#aa3493137c99adb7110b09791581e4b13',1,'rectangle::set_coordinates()'],['../classsquare.html#a20fd6155f90ef8e03885d3be6f3e0dfd',1,'square::set_coordinates()'],['../classtriangle.html#ab0d22c43b5639bcc4f88c719992ca678',1,'triangle::set_coordinates()']]],
+  ['set_5fgeometry_2',['set_geometry',['../classrectangle.html#a1615f4640d7a09a545cc9584b8d2cdf6',1,'rectangle::set_geometry()'],['../classtriangle.html#a6e721d53b8b24b6bc82fc255447f3a73',1,'triangle::set_geometry()']]],
+  ['set_5fline_5fcolor_3',['set_line_color',['../classcircle.html#ab2a5339954ca06536670be9372e5d181',1,'circle::set_line_color()'],['../classgraphic__primitive.html#a48ed2768a59f5a564073659c4b75c3bb',1,'graphic_primitive::set_line_color()'],['../classrectangle.html#a5b8f4e5fecbbc68d4229581596f5789a',1,'rectangle::set_line_color()'],['../classsquare.html#a72348f137272d73e09dd1bb096312ddf',1,'square::set_line_color()'],['../classtriangle.html#af38e421b110ff8a1c59ccdcc40031ce3',1,'triangle::set_line_color()']]],
+  ['set_5fradius_4',['set_radius',['../classcircle.html#aed163c24c1be54eedae61887a472642e',1,'circle']]],
+  ['set_5fsize_5',['set_size',['../classsquare.html#afbdeb1d8cd520316fd9f3e4aa43b7187',1,'square']]],
+  ['square_6',['square',['../classsquare.html#a382c43d2498cddd6405754635ca14bdd',1,'square']]]
+];
